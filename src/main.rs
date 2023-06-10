4e3db843e065266c2949d9e987a932c0ad2d923e
Machisté N. Quintana <mnquintana@users.noreply.github.com>
2023-06-10 17:58:51 -0400

    Repeatedly ask for grid size

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ mod grid;
 mod location;
 
 fn main() -> io::Result<()> {
-    println!("What size grid would you like to play on? (e.g. 3, for a 3x3 grid): ");
+    let grid_size = loop {
+        println!("What size grid would you like to play on? (e.g. 3, for a 3x3 grid): ");
 
-    let mut grid_size = String::new();
-    io::stdin().read_line(&mut grid_size)?;
+        let mut grid_size = String::new();
+        io::stdin().read_line(&mut grid_size)?;
 
-    let grid_size: u8 = grid_size.trim().parse().unwrap();
+        if let Ok(grid_size) = grid_size.trim().parse() {
+            break grid_size;
+        }
+    };
 
     let mut game = TicTacToe::new(grid_size);
     game.start()?;
